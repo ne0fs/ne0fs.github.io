975936d60e71fe955c6e730b030f35e376ae3c62
vang1ong7ang <vang1ong7ang@outlook.com>
2020-08-12 09:32:07 +0000

    dev

--- ne0fs.js
+++ ne0fs.js
@@ -39,7 +39,7 @@ fetch('uploader.wasm').then(response => response.arrayBuffer()).then(function (b
                             stderr += decoder.decode(buf);
                             const nl = stderr.lastIndexOf("\n");
                             if (nl != -1) {
-                                document.getElementById('status').innerText = stderr
+                                document.getElementById('status').innerText = stderr.substr(0, nl)
                                 stderr = stderr.substr(nl + 1);
                             }
                             return buf.length
@@ -55,54 +55,54 @@ fetch('uploader.wasm').then(response => response.arrayBuffer()).then(function (b
 
 fetch('downloader.wasm').then(response => response.arrayBuffer()).then(function (bin) {
     document.getElementById('download').onclick = function () {
+        const addr = document.getElementById('link').value
+        if (addr.substr(0, 8) != "ne0fs://") {
+            alert('not a ne0fs link')
+            return
+        }
+        const url = new URL('https://' + addr.substr(8))
         const go = new Go()
         WebAssembly.instantiate(bin, go.importObject).then((result) => {
-            const reader = new FileReader();
-            const files = document.getElementById('file').files
-            if (files.length == 0) {
-                alert('no file selected')
-                return
-            }
-            const file = files[0]
-            reader.readAsArrayBuffer(file)
-            reader.onloadend = function (e) {
-                var data = new Uint8Array(e.target.result)
-                global.fs.read = function (fd, buffer, offset, length, position, callback) {
-                    if (fd != 0) {
-                        const err = new Error("not implemented");
-                        err.code = "ENOSYS";
-                        callback(err);
-                        return
-                    }
-                    if (length > data.length) {
-                        length = data.length
-                    }
-                    buffer.set(data.slice(0, length), offset)
-                    data = data.slice(length)
-                    callback(null, length)
+            let stdout = new Uint8Array(0)
+            let stderr = ""
+            const decoder = new TextDecoder("utf-8");
+            global.fs.writeSync = function (fd, buf) {
+                switch (fd) {
+                    case 1:
+                        let last = stdout
+                        stdout = new Uint8Array(last.length + buf.length)
+                        stdout.set(last)
+                        stdout.set(buf, last.length)
+                        return buf.length
+                    case 2:
+                        stderr += decoder.decode(buf);
+                        const nl = stderr.lastIndexOf("\n");
+                        if (nl != -1) {
+                            console.log(stderr.substr(0, nl))
+                            document.getElementById('status').innerText = stderr.substr(0, nl)
+                            stderr = stderr.substr(nl + 1);
+                        }
+                        return buf.length
                 }
-                let stdout = ""
-                let stderr = ""
-                const decoder = new TextDecoder("utf-8");
-                global.fs.writeSync = function (fd, buf) {
-                    switch (fd) {
-                        case 1:
-                            stdout += decoder.decode(buf)
-                            document.getElementById('link').value = stdout
-                            return buf.length
-                        case 2:
-                            stderr += decoder.decode(buf);
-                            const nl = stderr.lastIndexOf("\n");
-                            if (nl != -1) {
-                                document.getElementById('status').innerText = stderr
-                                stderr = stderr.substr(nl + 1);
-                            }
-                            return buf.length
-                    }
-                }
-                go.argv = ["NE0FS", "-i"]
-                go.run(result.instance);
             }
+
+            go.argv = ["NE0FS", "-l", addr, "-o"]
+            go.exit = (code) => {
+                if (code !== 0) {
+                    alert("exit code:", code);
+                    return
+                }
+                let a = document.createElement("a");
+                document.body.appendChild(a);
+                a.style = "display: none";
+                let blob = new Blob([stdout], { type: 'octet/stream' })
+                let expr = window.URL.createObjectURL(blob)
+                a.href = expr
+                a.download = url.pathname.split('/').pop();
+                a.click();
+                window.URL.revokeObjectURL(url);
+            };
+            go.run(result.instance);
         });
     }
     document.getElementById('download').disabled = false
